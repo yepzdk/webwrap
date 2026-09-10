68b677e74a90f1986bbddfd4f2f77000f1ef764e
Jesper Pedersen <pejes@aarhus.dk>
2026-09-10 10:13:17 +0200

    chore: set webwrap.rb sha256 for the 0.9.1 release

--- dist/homebrew/webwrap.rb
+++ dist/homebrew/webwrap.rb
@@ -5,7 +5,7 @@ class Webwrap < Formula
   url "https://github.com/yepzdk/webwrap/releases/download/v0.9.1/webwrap-0.9.1-macos-universal.tar.gz"
   version "0.9.1"
   # From the release: webwrap-0.9.1-macos-universal.tar.gz.sha256
-  sha256 "9fe4513d4377d8050b91d6bb2d19d9bcb9a5d052be3ad7c17de897442397451f"
+  sha256 "1a1097139b60d934b9032329a06bfd9c26ce5a2b7ee732d8e99d6dbf4a09b390"
   license "MIT"
 
   depends_on :macos
